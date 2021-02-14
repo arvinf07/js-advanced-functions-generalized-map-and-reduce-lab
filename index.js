e39dc7485d53d634e7819cc8a51adfe7468878aa
Arvin Fernandez <arvinf07@gmail.com>
2021-02-14 15:02:35 -0500

    commit tree dd0ae0c702b1c9249b488f3aedea6e1232ba277c

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your functions here
+
+function map(array, block){
+  const newArray = []
+  array.forEach(e => {
+    newArray.push(block(e))
+  })
+  return newArray
+}
+
+function reduce(array, block, start){
+  debugger
+  if (start){
+    let result = start
+  }else{
+    let result = 0
+  }
+  array.forEach(e => {
+    result += block(e) 
+  })
+  return result
+}
